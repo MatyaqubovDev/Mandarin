28b22df38c297d094cc2b9685cb8d15d3642c827
Bogibek_Matyaqubov <uzroot.pdp@gmail.com>
2022-02-02 11:07:16 +0500

    valied

--- app/src/main/java/dev/matyaqubov/mandarin/activity/SignIn.kt
+++ app/src/main/java/dev/matyaqubov/mandarin/activity/SignIn.kt
@@ -2,11 +2,30 @@ package dev.matyaqubov.mandarin.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
 import dev.matyaqubov.mandarin.R
+import java.util.regex.Pattern
 
 class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        initViews()
+    }
+
+    private fun initViews() {
+
+        val et_email=findViewById<EditText>(R.id.et_email)
+        et_email.addTextChangedListener {
+            if (isValied(it.toString())){
+
+            }
+        }
+    }
+
+    private fun isValied(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 }
